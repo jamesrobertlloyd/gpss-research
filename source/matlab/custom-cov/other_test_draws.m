6bef489bf3312370872f9d58835af477df2905cd
James Lloyd <james.robert.lloyd@gmail.com>
2013-09-22 20:17:54 +0100

    Playing with spectral kernels
    Now understand them as smoothly varying amplitude and phase (which
    looks like smoothly varying period)

--- source/matlab/custom-cov/other_test_draws.m
+++ source/matlab/custom-cov/other_test_draws.m
@@ -116,3 +116,106 @@ K = K + 1e-9*max(max(K))*eye(size(K));
 y = chol(K)' * randn(size(x));
 
 plot(x, y);
+
+%% SE * Cos
+
+x = linspace(0, 1, 1000)';
+
+cov_func = {@covProd, {@covSEiso, @covCos}};
+hyp.cov = [-3,0,-3,0];
+
+K = feval(cov_func{:}, hyp.cov, x);
+K = K + 1e-9*max(max(K))*eye(size(K));
+
+y = chol(K)' * randn(size(x));
+
+plot(x, y);
+
+%% Cosine fit
+
+x = linspace(-5, 5, 250)';
+%y = cos(1.8*pi*x) + 0.1*randn(size(x));
+y = cos(2*pi*x) + 0.01*randn(size(x));
+
+cov_func = {@covCos};
+hyp.cov = [0,0];
+
+mean_func = @meanZero;
+hyp.mean = [];
+
+lik_func = @likGauss;
+hyp.lik = log(std(y) / 10);
+
+hyp = minimize(hyp, @gp, -100, @infExact, mean_func, cov_func, lik_func, x, y);
+
+x_test = linspace(-5, 10, 1000)';
+
+fit = gp(hyp, @infExact, mean_func, cov_func, lik_func, x, y, x_test);
+
+plot(x, y, 'o');
+hold on;
+plot(x_test, fit);
+hold off;
+
+%% SE * Cosine fit
+
+x = linspace(-5, 5, 500)';
+y = 3*(cos(2*pi*(x+0.00*x.*x)).*(x>=0)+cos(1.0*2*pi*(x+0.00*x.*x)).*(x<0)) + 0.01*randn(size(x));
+%x = x + 0.01*sin(2*pi*x/1.5);
+%x = 2*x;
+
+cov_func = {@covProd, {@covSEiso, @covCos}};
+hyp.cov = [0,0,0,0];
+
+mean_func = @meanZero;
+hyp.mean = [];
+
+lik_func = @likGauss;
+hyp.lik = log(std(y) / 10);
+
+hyp = minimize(hyp, @gp, -100, @infExact, mean_func, cov_func, lik_func, x, y);
+
+%%
+
+x_test = linspace(-10, 10, 1000)';
+
+fit = gp(hyp, @infExact, mean_func, cov_func, lik_func, x, y, x_test);
+
+K = feval(cov_func{:}, hyp.cov, x) + eye(length(x))*exp(2*hyp.lik);
+K = K + 1e-9*max(max(K))*eye(size(K));
+K_star = feval(cov_func{:}, hyp.cov, x, x_test);
+K_starstar = feval(cov_func{:}, hyp.cov, x_test, x_test);
+K = K_starstar - K_star'*(K \ K_star);
+K = K + 1e-9*max(max(K))*eye(size(K));
+
+plot(x, y, 'o');
+hold on;
+plot(x_test, fit);
+plot(x_test, fit + chol(K)' * randn(size(x_test)), 'g');
+plot(x_test, fit + chol(K)' * randn(size(x_test)), 'r');
+hold off;
+
+%% Flexible periodic function with fixed period
+
+x = linspace(-20, 20, 1000)';
+y = zeros(size(x));
+y1 = y;
+y2 = y;
+
+cov_func = {@covSEiso};
+hyp.cov = [log(3),0];
+
+K = feval(cov_func{:}, hyp.cov, x);
+K = K + 1e-9*max(max(K))*eye(size(K));
+chol_K = chol(K);
+
+for i = 1:1
+    y1 = y1 + (chol_K' * randn(size(x))).*cos(2*pi*i*x)./(i*i) + ...
+              (chol_K' * randn(size(x))).*sin(2*pi*i*x)./(i*i);
+    y2 = y2 + (chol_K' * randn(size(x))).*cos(2*pi*i*x)./(i*i) + ...
+              (chol_K' * randn(size(x))).*sin(2*pi*i*x)./(i*i);
+end
+plot(x, y1, 'r');
+hold on;
+plot(x, y2, 'g');
+hold off;

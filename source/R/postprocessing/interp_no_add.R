77cd6058bd9010bc15a7aee7846a2270b1304226
James Robert Lloyd <jrl44@cam.ac.uk>
2014-01-14 12:12:45 +0000

    Updated eureqa interp results in paper

--- source/R/postprocessing/interp_no_add.R
+++ source/R/postprocessing/interp_no_add.R
@@ -9,6 +9,6 @@ xcoord[X2$ind=="SE"]<- 4
 xcoord[X2$ind=="CP"]<- 5
 xcoord[X2$ind=="EL"]<- 6
 
-boxplot(X, ylim=c(1,1.8))
+boxplot(X, ylim=c(1,3))
 par(new=T)
-plot(xcoord, (X2$values), xlim=c(0.5,6.5), ylim=c(1,1.8), axes=F, ylab="Standardised RMSE", xlab="")
+plot(xcoord, (X2$values), xlim=c(0.5,6.5), ylim=c(1,3), axes=F, ylab="Standardised RMSE", xlab="")

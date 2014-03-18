bae5a23e160ba7ac25b8f84b10412b036f475029
David Duvenaud <dkd23@cam.ac.uk>
2014-03-18 18:47:04 +0000

    Fixed a minor problem with dates in Quebec births datasets

--- data/quebec/process.m
+++ data/quebec/process.m
@@ -1,23 +1,28 @@
 %% Load data
 
-data = csvread('quebec-births-standard.csv', 1, 0);
+data = csvread('quebec-births-fixed.csv', 1, 0);
 X_full = data(:,1:4);
 y_full = data(:,5);
 
 %% Plot
-
 plot(X_full(:,1), y_full, 'o');
 
+% Set random seed.
+randn('state',0);
+rand('state',0);
+
 %% Subsample
+for n = [1000,2000,5000]
+    rp = randperm(length(y_full));
+    idx = rp(1:n);
 
-n = 5000;
+    X = X_full(idx,:);
+    y = y_full(idx,:);
 
-rp = randperm(length(y_full));
-idx = rp(1:n);
+    save(['quebec-', int2str(n), '.mat'], 'X', 'y');
+end
 
-X = X_full(idx,:);
-y = y_full(idx,:);
+X = X_full;
+y = y_full;
 
-save(['quebec-', int2str(n), '.mat'], 'X', 'y');
-
-plot(X(:,1), y, 'o');
+save('quebec-all.mat', 'X', 'y');
